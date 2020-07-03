6dbdc5ec7373bbe6bba5d37d10392d92b7ffee6b
AymDev <aymericmayeux@gmail.com>
2020-07-03 14:21:56 +0200

    AJOUTE la création de catégories en Back-Office

--- src/Controller/AdminCategoryController.php
+++ src/Controller/AdminCategoryController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Form\CategoryFormType;
 use App\Repository\CategoryRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -22,6 +25,28 @@ class AdminCategoryController extends AbstractController
 
         return $this->render('admin_category/index.html.twig', [
             'category_list' => $categories,
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="add")
+     */
+    public function add(Request $request, EntityManagerInterface $entityManager)
+    {
+        $form = $this->createForm(CategoryFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $category = $form->getData();
+            $entityManager->persist($category);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Nouvelle catégorie enregistrée.');
+            return $this->redirectToRoute('admin_category_list');
+        }
+
+        return $this->render('admin_category/add.html.twig', [
+            'category_form' => $form->createView()
         ]);
     }
 }
